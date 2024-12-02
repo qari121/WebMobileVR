91cad703de446246eeb3c902d0c79adc007d130f
qari121 <qaribinsaeed@gmail.com>
2024-12-03 02:43:02 +0500

    updated paths

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/slopes2025/',
   build: {
     assetsDir: 'assets',
     rollupOptions: {
