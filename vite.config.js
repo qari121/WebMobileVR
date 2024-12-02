7d93920d4eeddd5060252a6635763712ad2878be
qari121 <qaribinsaeed@gmail.com>
2024-12-03 02:22:46 +0500

    chnaged loading page

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/', // Ensure paths are relative to this subdirectory
-  
+  base: '/slopes2025/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      }
+    }
+  },
   plugins: [react()],
+  server: {
+    open: '/index.html'
+  }
 })
