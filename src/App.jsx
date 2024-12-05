2e5f0d4837e141f868d12c7b830a749fb4c3d2b3
qari121 <qaribinsaeed@gmail.com>
2024-12-05 23:24:04 +0500

    changed font

--- src/App.jsx
+++ src/App.jsx
@@ -155,7 +155,7 @@ function App() {
     let textMesh1;
     const fontLoader = new FontLoader();
     fontLoader.load(
-      '/src/assets/texas.json',
+      '/src/Source Sans 3 ExtraLight_Regular.json',
       (font) => {
         const textGeometry = new TextGeometry("It's not what you look at that matters:", {
           font,
